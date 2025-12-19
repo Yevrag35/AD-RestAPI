8a2b89a1a8abef05f6e8e714a53a9c4093f98444
Mike Garvey <mike.garvey@yevrag35.com>
2025-12-19 10:36:06 -0500

    Update NuGet packages, logging, and environment setup
    Upgraded Swashbuckle.AspNetCore and Microsoft.Identity.Web to latest versions.
    Added NLog and OpenAPI packages to AD.Api-Minimal.
    Refactored Program.cs to use AD.Api.Startup and NLog logging.
    Introduced WebEnv.cs for centralized environment config.
    Minor formatting and reference improvements in AD.Api.Core.csproj.

--- AD.Api-Minimal/Program.cs
+++ AD.Api-Minimal/Program.cs
@@ -1,2 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
+using AD.Api.Startup;
+using NLog.Extensions.Logging;
+using WebApp = Microsoft.AspNetCore.Builder.WebApplication;
+
+var builder = WebApp.CreateBuilder(new WebApplicationOptions
+{
+	Args = args,
+	EnvironmentName = AD.Api.WebEnv.EnvironmentName,
+	ContentRootPath = AppDomain.CurrentDomain.BaseDirectory,
+})
+.SetupEnvironment();
+
+builder.Host.ConfigureHostOptions(o => o.ServicesStartConcurrently = true);
+
+builder.Logging.ClearProviders()
+			   .SetMinimumLevel(builder.Configuration.GetRequiredSection("Logging").GetRequiredSection("LogLevel").GetValue("Default", LogLevel.Information))
+			   .Configure(o =>
+				{
+					o.ActivityTrackingOptions =
+						ActivityTrackingOptions.TraceId |
+						ActivityTrackingOptions.SpanId |
+						ActivityTrackingOptions.ParentId;
+				})
+				.AddNLog(new NLogProviderOptions
+				{
+					IncludeScopes = true,
+					CaptureMessageProperties = true,
+					AutoShutdown = true,
+					CaptureMessageTemplates = true,
+				});
 
---
+++ AD.Api-Minimal/WebEnv.cs
@@ -0,0 +1,27 @@
+﻿namespace AD.Api;
+
+internal static class WebEnv
+{
+#if DEBUG
+	internal static readonly bool IsDebugBuild = true;
+	internal static readonly string EnvironmentName = Microsoft.Extensions.Hosting.Environments.Development;
+	internal static readonly bool SerializeIndent = EnvironmentWantsIndents();
+#else
+    internal static readonly bool IsDebugBuild = false;
+    internal static readonly string EnvironmentName = Microsoft.Extensions.Hosting.Environments.Production;
+    internal static readonly bool SerializeIndent = true;
+#endif
+
+	private static bool EnvironmentWantsIndents()
+	{
+		return HasVariableValue("API_SERIALIZE", "Indent");
+	}
+
+	private static bool HasVariableValue(string variableName, string mustEqual)
+	{
+		string? value = Environment.GetEnvironmentVariable(variableName, EnvironmentVariableTarget.Process);
+		return value is not null
+			   &&
+			   StringComparer.CurrentCulture.Equals(mustEqual, value);
+	}
+}
